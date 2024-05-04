b7ee88457a87d5979f4637959231696a7ab9def3
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-05-04 19:32:35 +0100

    meets list

--- src/components/meets/MeetItem.js
+++ src/components/meets/MeetItem.js
@@ -5,19 +5,19 @@ import classes from './MeetItem.module.css'
 export default function MeetItem({ title, address, description, imgUrl }) {
   return (
     <li className={classes.Li}>
+          <div className={classes.IconTitleDiv}>
+          <h3 className={classes.infoTitle}>{title}</h3>
+        {/* <FaStar onClick={()=>{}} className={classes.Icon} size={20} color='#86c5af'/> */}
+        <FaRegStar onClick={()=>{}} className={classes.Icon} size={30} color='#86c5af'/>
+      </div>
       <div className={classes.imageDiv}>
-        <img className={classes.image} src={imgUrl} alt={title} width={300} height={300}/>
+        <img className={classes.image} src={imgUrl} alt={title}/>
       </div>
 
       <div className={classes.infoDiv}>
-        <h3 className={classes.infoTitle}>{title}</h3>
+        
         <address className={classes.infoAdress}>{address}</address>
         <p className={classes.infoDescription}>{description}</p>
-      </div>
-
-      <div className={classes.IconDiv}>
-        <FaStar onClick={()=>{}} className={classes.Icon} size={20} color='#86c5af'/>
-        <FaRegStar onClick={()=>{}} className={classes.Icon} size={20} color='#86c5af'/>
       </div>
     </li>
   )
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,12 +1,12 @@
 import MeetItem from "../components/meets/MeetItem";
 import { DUMMY_DATA } from "../components/meets/meetsList";
-
+import classes from './AllMeetup.module.css'
 
 export default function AllMeetups() {
   return (
     <section>
-      <h1>All Meets</h1>
-      <ul>
+      <h1 className={classes.allMeetsHeader}>All Meets</h1>
+      <ul className={classes.meetsList}>
         {DUMMY_DATA.map((meet) => (
           <MeetItem title={meet.title} description={meet.description} address={meet.address} imgUrl={meet.image} key={meet.id} />
         ))}
