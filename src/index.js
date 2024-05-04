d0dc7c8ad84262bfdcc01f5f2db0ce392c5ac3fa
Abderrahmane-belarbi <abdoustart007@gmail.com>
2024-05-04 11:43:32 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import Application from './Application';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Application />
   </React.StrictMode>
 );
